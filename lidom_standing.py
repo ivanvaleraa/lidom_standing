b264d4a9d65ef86411ffacf7f84257560b99d480
Ivan Valera <ivanvaleraa@gmail.com>
2024-10-18 17:21:54 -0400

    replacing pretty with pd dataframe

--- lidom_standing.py
+++ lidom_standing.py
@@ -4,21 +4,15 @@ import requests
 import streamlit as st
 from bs4 import BeautifulSoup
 from prettytable import PrettyTable
+import pandas as pd
 
 
 url = 'https://estadisticas.lidom.com/'
 respuesta = requests.get(url)
-if respuesta.status_code == 200:
-  total_teams = 6
-  soup = BeautifulSoup(respuesta.text, 'html.parser')
-  ##print(x)
-  W = soup.find_all('table')[1].find_all('tr')[1].find_all('td')[1].get_text().strip() 
-  L = soup.find_all('table')[1].find_all('tr')[1].find_all('td')[2].get_text().strip() 
-  PCT = soup.find_all('table')[1].find_all('tr')[1].find_all('td')[3].get_text().strip() 
-  
-  
-headers = ("Equipo", "W - L", "PCT")
-standing = PrettyTable(headers)
+columns = ["Equipo", "W - L", "PCT"]
+##standing = PrettyTable(headers)
+
+rows = []
 
 if respuesta.status_code == 200:
   total_teams = 6
@@ -26,22 +20,15 @@ if respuesta.status_code == 200:
 
   for x in range(total_teams):
     x += 1
-    ##print(x)
-    equipo = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[0].find_all('a')[0].get_text().strip() ##nombre del equipo
-    w = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[2].get_text().strip() 
-    l = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[3].get_text().strip() 
-    wl = w + " - " + l
-    pct = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[4].get_text().strip() 
-  
-  
-    standing.add_row([equipo, wl, pct])
+    team_name = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[0].find_all('a')[0].get_text().strip()
+    wins_loses = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[2].get_text().strip() + " - " + soup.find_all('table')[1].find_all('tr')[x].find_all('td')[3].get_text().strip()
+    pct = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[4].get_text().strip()
+    rows.append([team_name,wins_loses,pct])
 
-
-standing.padding_width = 5
-standing.border = True
-standing.sortby = 'PCT'
-
-standing.align['Equipo'] = 'l'
-standing.align['PCT'] = 'r'
-standing.reversesort = True
-st.write(standing)
+df = pd.DataFrame(rows, columns=columns)
+df.set_index("Equipo", inplace=True)
+# df.style.set_table_styles({
+#     'Equipo': [{'selector': '',
+#            'props': [('color','red'),('width', '200px')]}],
+# }) not working
+st.write(df)
