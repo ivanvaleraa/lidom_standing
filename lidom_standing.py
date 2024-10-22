cd4f6bfbd38b530be9b895b19a61ff168e3aff58
Ivan Valera <ivanvaleraa@gmail.com>
2024-10-22 00:12:07 -0400

    adding some titles and new table

--- lidom_standing.py
+++ lidom_standing.py
@@ -8,13 +8,19 @@ import pandas as pd
 from decimal import Decimal
 
 
+
+st.title(":blue[LI]:red[DOM] Temporada 2024-2025 :flag-do::baseball:")
+st.header("Tabla de Posiciones")
+
 url = 'https://estadisticas.lidom.com/'
 url2 = 'https://estadisticas.lidom.com/Colectivo'
 respuesta = requests.get(url)
 respuesta2 = requests.get(url2)
 
-columns = ["Equipo", "W - L", "PCT", "K%", "BB%"]
-rows = []
+columns_standing = ["Equipo", "JJ", "G - P", "PCT", "JD","Casa", "Ruta", "Racha", "U10"]
+columns_offensive_stats = ["Equipo","AVG", "OBP", "SLG", "OPS", "K%", "BB%"]
+rows_standing = []
+rows_offensive_stats = []
 total_teams = 6
 
 ##aqui ponemos las estadisticas basicas
@@ -24,15 +30,28 @@ if respuesta.status_code == 200:
 
   for x in range(total_teams):
     x += 1
+    jj = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[1].get_text().strip()
     team_name = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[0].find_all('a')[0].get_text().strip()
     wins_loses = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[2].get_text().strip() + " - " + soup.find_all('table')[1].find_all('tr')[x].find_all('td')[3].get_text().strip()
     pct = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[4].get_text().strip()
+    gb = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[5].get_text().strip()
+    casa = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[6].get_text().strip()
+    ruta = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[7].get_text().strip()
+    racha = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[8].get_text().strip()
+    u10 = soup.find_all('table')[1].find_all('tr')[x].find_all('td')[9].get_text().strip()
 
     ##segundo
-    ab = int(soup2.find_all('table')[1].find_all('tr')[x].find_all('td')[2].get_text().strip())
+    team_name = soup2.find_all('table')[1].find_all('tr')[x].find_all('td')[0].find_all('a')[0].get_text().strip()
 
+    ab = int(soup2.find_all('table')[1].find_all('tr')[x].find_all('td')[2].get_text().strip())
     k = int(soup2.find_all('table')[1].find_all('tr')[x].find_all('td')[11].get_text().strip())
     bb = int(soup2.find_all('table')[1].find_all('tr')[x].find_all('td')[9].get_text().strip())
+
+    avg = (soup2.find_all('table')[1].find_all('tr')[x].find_all('td')[15].get_text().strip())
+    obp = (soup2.find_all('table')[1].find_all('tr')[x].find_all('td')[16].get_text().strip())
+    slg = (soup2.find_all('table')[1].find_all('tr')[x].find_all('td')[17].get_text().strip())
+    ops = (soup2.find_all('table')[1].find_all('tr')[x].find_all('td')[18].get_text().strip())
+
     pa = ab + bb
 
     k_rate = ((k/pa)*100)
@@ -42,12 +61,14 @@ if respuesta.status_code == 200:
     k_rate = (format(k_rate,'.1f'))+"%"
     bb_rate = (format(bb_rate,'.1f'))+"%"
 
-    rows.append([team_name,wins_loses,pct,k_rate,bb_rate])
+    rows_standing.append([team_name, jj, wins_loses, pct, gb, casa, ruta, racha, u10])
+    rows_offensive_stats.append([team_name,avg,obp,slg,ops,k_rate,bb_rate])
 
-df = pd.DataFrame(rows, columns=columns)
-df.set_index("Equipo", inplace=True)
-# df.style.set_table_styles({
-#     'Equipo': [{'selector': '',
-#            'props': [('color','red'),('width', '200px')]}],
-# }) not working
-st.write(df)
+df_standing = pd.DataFrame(rows_standing, columns=columns_standing)
+df_offensive_stats = pd.DataFrame(rows_offensive_stats, columns=columns_offensive_stats)
+
+
+
+st.dataframe(df_standing, width=2000, hide_index=True)
+st.header("Estadísticas Ofensivas")
+st.dataframe(df_offensive_stats, width=2000, hide_index=True)
